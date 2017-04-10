fe47d463eda26ad679d39cf4b943d8069f035098
dwiindrawati <dwiindrawati04@gmail.com>
2017-04-10 12:30:23 +0000

    Web Blog V.1

--- form_add.php
+++ form_add.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 
 <head>
 
@@ -16,9 +16,6 @@
 
     <!-- Theme CSS -->
     <link href="assets/css/clean-blog.min.css" rel="stylesheet">
-
-    <!-- custom css -->
-    <link href="assets/css/custom.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
     <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -80,15 +77,36 @@
 
     <!-- Main Content -->
     <div class="container">
-        <table class="table">
-            <tr>
-                <td><a class="tbl-title" href="">Reality wasn't really reality</a></td>
-                <td class="tbl-date">10 April 2017</td>
-                <td><a class="btn-action publish" href="">publish</a></td>
-                <td><a class="btn-action" href="">edit</a></td>
-                <td><a class="btn-action" href="">hapus</a></td>
-            </tr>
-        </table>
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+
+                <form action="addpost.php" method="post" novalidate >
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label>Judul</label>
+                            <input type="text" class="form-control" placeholder="Judul" id="name" name="postTitle" required data-validation-required-message="Please enter your name.">
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+
+
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label>Isi</label>
+                            <textarea rows="5" class="form-control" placeholder="Isi" id="message" name="postContent" required data-validation-required-message="Please enter a message."></textarea>
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="form-group col-xs-12">
+                            <button type="submit" class="btn btn-default" name="submit">Send</button>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
     </div>
 
     <hr>
